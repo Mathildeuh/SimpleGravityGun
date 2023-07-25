13808b831e2c403f1c451dc20e4063da0270d622
Mathildeuh <76065396+Mathildeuh@users.noreply.github.com>
2023-07-26 00:02:49 +0200

    Move ItemBuilder in SimpleGravityGun.java

--- src/main/java/fr/simplegravitygun/commands/GravityGunCommand.java
+++ src/main/java/fr/simplegravitygun/commands/GravityGunCommand.java
@@ -1,16 +1,18 @@
 package fr.simplegravitygun.commands;
 
-import fr.simplegravitygun.utils.ItemBuilder;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import fr.simplegravitygun.SimpleGravityGun;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class GravityGunCommand implements CommandExecutor {
+
+    private final SimpleGravityGun plugin;
+
+    public GravityGunCommand(SimpleGravityGun simpleGravityGun) {
+        this.plugin = simpleGravityGun;
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -18,16 +20,9 @@ public class GravityGunCommand implements CommandExecutor {
             return false;
         }
 
-        ItemStack gravityGun = new ItemBuilder(Material.STICK)
-                .setName(ChatColor.translateAlternateColorCodes('&', "&fGravityGun"))
-                .setLore(
-                        ChatColor.translateAlternateColorCodes('&', "&7Clique droit pour déplacer un bloc"),
-                        ChatColor.translateAlternateColorCodes('&',"&7Clique droit en étant accroupi pour annuler")
-                )
-                .toItemStack();
 
         // Give the gravity gun to the player
-        player.getInventory().addItem(gravityGun);
+        player.getInventory().addItem(plugin.gravityGun);
 
         return true;
     }
