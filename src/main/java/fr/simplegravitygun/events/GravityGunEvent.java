6694951c1dc3bc0a8ca693f974fd7e044378d19b
Mathildeuh <76065396+Mathildeuh@users.noreply.github.com>
2023-07-26 00:03:33 +0200

    Check if stick is the right one, add cooldown and remove some shits

--- src/main/java/fr/simplegravitygun/events/GravityGunEvent.java
+++ src/main/java/fr/simplegravitygun/events/GravityGunEvent.java
@@ -1,5 +1,6 @@
 package fr.simplegravitygun.events;
 
+import fr.simplegravitygun.SimpleGravityGun;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.FallingBlock;
@@ -8,18 +9,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashMap;
+
 public class GravityGunEvent implements Listener {
-    private final JavaPlugin plugin;
+    private final SimpleGravityGun plugin;
+    private final HashMap<Player, FallingBlock> fallingBlockHashMap = new HashMap<>();
     private Player lastPlayer;
     private Block lastBlock;
 
-    public GravityGunEvent(JavaPlugin plugin) {
+    public GravityGunEvent(SimpleGravityGun plugin) {
         this.plugin = plugin;
-        this.lastPlayer = null;
-        this.lastBlock = null;
     }
 
     /**
@@ -33,16 +34,33 @@ public class GravityGunEvent implements Listener {
         Block block = event.getClickedBlock();
 
         // Vérifie si le joueur a le "gravity gun" dans la main
-        if (player.getInventory().getItemInMainHand().getType() == Material.STICK) {
-            // Vérifie si le bloc cliqué n'est pas nul
+
+        if (player.getItemInHand() != null && player.getItemInHand().hasItemMeta() && player.getItemInHand().getItemMeta().hasDisplayName() && player.getInventory().getItemInHand().getItemMeta().getDisplayName().equals(plugin.gravityGun.getItemMeta().getDisplayName())) {
+
+            if (plugin.cooldown.containsKey(player)) {
+                return;
+            }
+            plugin.cooldown.remove(player);
+            plugin.cooldown.put(player, 1);
+
             if (block != null) {
                 if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
                     if (lastPlayer == null || lastPlayer != player || lastBlock != block) {
-                        FallingBlock fallingBlock = block.getWorld().spawn(block.getLocation(), FallingBlock.class);
+
+                        if (fallingBlockHashMap.containsKey(player)) {
+                            player.sendMessage("§cVous devez d'abord annuler le bloc précédent !");
+                            return;
+                        }
+                        FallingBlock fallingBlock = block.getWorld().spawn(block.getLocation().add(0, 0.2, 0), FallingBlock.class);
                         fallingBlock.setGravity(false);
-                        fallingBlock.setDropItem(false);
+                        fallingBlock.setDropItem(true);
                         fallingBlock.setHurtEntities(false);
                         fallingBlock.setInvulnerable(true);
+                        fallingBlock.setCustomName("Gravity Gun - " + block.getType().name().toLowerCase());
+                        fallingBlock.setCustomNameVisible(true);
+                        fallingBlock.setFreezeTicks(999999999);
+                        player.sendMessage("§aBloc capturé !");
+                        fallingBlockHashMap.put(player, fallingBlock);
 
                         block.setType(Material.AIR);
 
@@ -51,14 +69,28 @@ public class GravityGunEvent implements Listener {
                             public void run() {
                                 // Vérifie si le joueur est accroupi
                                 if (player.isSneaking()) {
-                                    fallingBlock.remove();
                                     block.setType(fallingBlock.getBlockData().getMaterial());
+                                    fallingBlock.remove();
+                                    fallingBlockHashMap.remove(player);
+                                    player.sendMessage("§cBloc annulé !");
                                     cancel();
+                                    return;
                                 } else {
                                     // Calcule la position cible et la vélocité
-                                    org.bukkit.Location targetLocation = player.getLocation().add(player.getLocation().getDirection().multiply(2.5)).add(0, 1, 0);
+                                    org.bukkit.Location targetLocation = player.getLocation().add(player.getLocation().getDirection().multiply(2.5)).add(0, 1.2, 0);
                                     org.bukkit.util.Vector velocity = targetLocation.subtract(fallingBlock.getLocation()).toVector();
                                     fallingBlock.setVelocity(velocity);
+                                }
+                                if (fallingBlock.isDead()) {
+                                    fallingBlockHashMap.remove(player);
+                                    player.sendMessage("§aBloc posé !");
+                                    cancel();
+                                }
+                                if (!player.getItemInHand().getItemMeta().getAsString().equals(plugin.gravityGun.getItemMeta().getAsString())) {
+                                    fallingBlock.remove();
+                                    fallingBlockHashMap.remove(player);
+                                    player.sendMessage("§cVous avez lacher le Gravity Gun !");
+                                    cancel();
                                 }
                             }
                         }.runTaskTimer(plugin, 0, 1);
@@ -66,22 +98,10 @@ public class GravityGunEvent implements Listener {
                         lastPlayer = player;
                         lastBlock = block;
                     }
-                } else if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                    // Vérifie si le bloc n'est pas déjà pris
-                    if (block == lastBlock) {
-                        org.bukkit.util.Vector velocity = player.getLocation().getDirection().multiply(8);
-                        block.getWorld().spawnFallingBlock(block.getLocation(), block.getBlockData()).setVelocity(velocity);
-                    }
                 }
-            }
-        } else {
-            // Si le joueur n'a pas le "gravity gun" dans la main, le bloc revient à sa position initiale
-            if (block != null && block.getType() != Material.AIR) {
-                block.setType(block.getType());
             }
         }
     }
-
 
 }
 
