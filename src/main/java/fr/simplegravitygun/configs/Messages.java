0fde502fa301a8731eef850ee60c30a6055d0744
Mathildeuh <76065396+Mathildeuh@users.noreply.github.com>
2023-07-26 15:22:59 +0200

    Optimizations, Fixes, More Configs, Major Update

--- src/main/java/fr/simplegravitygun/configs/Messages.java
+++ src/main/java/fr/simplegravitygun/configs/Messages.java
@@ -1,45 +1,39 @@
 package fr.simplegravitygun.configs;
-
 import fr.simplegravitygun.SimpleGravityGun;
 import org.bukkit.ChatColor;
-
 public class Messages {
-
-    SimpleGravityGun plugin;
     static String block_catch;
     static String block_canceled;
     static String block_placed;
+    static String block_placed_sneak;
     static String gun_released;
-    static  String already_using_gun;
-
+    static String already_using_gun;
+    SimpleGravityGun plugin;
     public Messages(SimpleGravityGun plugin) {
         this.plugin = plugin;
-
         block_catch = plugin.getConfig().getString("messages.block-catch");
-        block_canceled = plugin.getConfig().getString("messages.block-canceled");
+        block_placed_sneak = plugin.getConfig().getString("messages.block-placed-sneak");
         block_placed = plugin.getConfig().getString("messages.block-placed");
         gun_released = plugin.getConfig().getString("messages.gun-released");
         already_using_gun = plugin.getConfig().getString("messages.already-using-gun");
 
     }
-
+    public static String getBlock_placed_sneak() {
+        return ChatColor.translateAlternateColorCodes('&', block_placed_sneak);
+    }
     public static String getAlready_using_gun() {
         return ChatColor.translateAlternateColorCodes('&', already_using_gun);
     }
-
     public static String getBlock_catch() {
         return ChatColor.translateAlternateColorCodes('&', block_catch);
     }
-
     public static String getBlock_canceled() {
-        return ChatColor.translateAlternateColorCodes('&',block_canceled);
+        return ChatColor.translateAlternateColorCodes('&', block_canceled);
     }
-
     public static String getBlock_placed() {
-        return ChatColor.translateAlternateColorCodes('&',block_placed);
+        return ChatColor.translateAlternateColorCodes('&', block_placed);
     }
-
     public static String getGun_released() {
-        return ChatColor.translateAlternateColorCodes('&',gun_released);
+        return ChatColor.translateAlternateColorCodes('&', gun_released);
     }
 }
--- src/main/java/fr/simplegravitygun/events/GravityGunEvent.java
+++ src/main/java/fr/simplegravitygun/events/GravityGunEvent.java
@@ -17,29 +17,20 @@ import java.text.DecimalFormat;
 import java.util.HashMap;
 
 public class GravityGunEvent implements Listener {
+    public static final HashMap<Player, FallingBlock> fallingBlockHashMap = new HashMap<>();
     private final SimpleGravityGun plugin;
-    private final HashMap<Player, FallingBlock> fallingBlockHashMap = new HashMap<>();
-    private Player lastPlayer;
-    private Block lastBlock;
-
     public GravityGunEvent(SimpleGravityGun plugin) {
         this.plugin = plugin;
     }
-
     Vector getDirectionBetweenLocations(Location Start, Location End) {
         Vector from = Start.toVector();
         Vector to = End.toVector();
         return to.subtract(from);
     }
-
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {
         Player player = e.getPlayer();
         Block block = e.getClickedBlock();
-
-
-        // Vérifie si le joueur a le "gravity gun" dans la main
-
         if (player.getItemInHand() != null && player.getItemInHand().getType() == plugin.gravityGun.getType() && player.getItemInHand().hasItemMeta() && player.getItemInHand().getItemMeta().hasDisplayName() && player.getInventory().getItemInHand().getItemMeta().getDisplayName().equals(plugin.gravityGun.getItemMeta().getDisplayName())) {
             e.setCancelled(true);
             if (plugin.cooldown.containsKey(player)) {
@@ -47,97 +38,87 @@ public class GravityGunEvent implements Listener {
             }
             plugin.cooldown.remove(player);
             plugin.cooldown.put(player, 1);
-
             if (block != null) {
-                 if(block.isLiquid() || block.isPassable() || block.isEmpty()) return;
-
+                if (block.isLiquid() || block.isPassable() || block.isEmpty()) return;
                 if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                    if (lastPlayer == null || lastPlayer != player || lastBlock != block) {
-
-                        if (fallingBlockHashMap.containsKey(player)) {
-                            player.sendMessage(Messages.getAlready_using_gun());
-                            return;
-                        }
-                        FallingBlock fallingBlock = block.getWorld().spawn(block.getLocation().add(0, 0.2, 0), FallingBlock.class);
-                        fallingBlock.setGravity(false);
-                        fallingBlock.setDropItem(true);
-                        fallingBlock.setHurtEntities(false);
-                        fallingBlock.setInvulnerable(true);
-                        fallingBlock.setCustomNameVisible(true);
-                        fallingBlock.setFreezeTicks(999999999);
-                        player.sendMessage(Messages.getBlock_catch());
-                        fallingBlockHashMap.put(player, fallingBlock);
-
-                        player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_YES, 1, 1);
-
-                        block.setType(Material.AIR);
-
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                // Vérifie si le joueur est accroupi
-                                if (player.isSneaking() && player.getGameMode() != GameMode.CREATIVE) {
-                                    destroyFallingBlock(fallingBlock, player);
-
-                                    player.sendMessage(Messages.getBlock_canceled());
-                                    cancel();
-                                    return;
-                                } else {
-                                    // Calcule la position cible et la vélocité
-                                    if (plugin.lazerEnabled()) {
-                                        Location loc1 = fallingBlock.getLocation();
-                                        Location loc2 = player.getLocation().add(0, 0.9, 0);
-                                        Vector vector = getDirectionBetweenLocations(loc1, loc2).normalize(); //make sure it has length one at start
-                                        for (double i = 0; i <= loc1.distance(loc2); i += 0.1) {
-                                            Vector addition = new Vector().copy(vector).multiply(i);
-                                            Location newLoc = loc1.clone().add(addition);
-                                            loc1.getWorld().spawnParticle(Particle.REDSTONE, newLoc, 0, 0, 2, 0, 1, new Particle.DustOptions(plugin.getRGBFromConfig(), 0.5F));
-                                        }
-                                    }
-
-                                    org.bukkit.Location targetLocation = player.getLocation().add(player.getLocation().getDirection().multiply(2.5)).add(0, 1.2, 0);
-                                    org.bukkit.util.Vector velocity = targetLocation.subtract(fallingBlock.getLocation()).toVector();
-                                    fallingBlock.setVelocity(velocity);
-                                    Double x = fallingBlock.getLocation().getX();
-                                    Double y = fallingBlock.getLocation().getY();
-                                    Double z = fallingBlock.getLocation().getZ();
-
-                                    DecimalFormat f = new DecimalFormat("##.00");
-//                                    System.out.println();
-
-                                    fallingBlock.setCustomName("Gravity Gun - " + f.format(x) + "/" + f.format(y) + "/" + f.format(z));
-
-                                }
-                                if (fallingBlock.isDead()) {
-                                    fallingBlockHashMap.remove(player);
-                                    player.sendMessage(Messages.getBlock_placed());
-                                    cancel();
-                                }
-                                if (!(player.getItemInHand() != null && player.getItemInHand().getType() == plugin.gravityGun.getType() && player.getItemInHand().hasItemMeta() && player.getItemInHand().getItemMeta().hasDisplayName() && player.getInventory().getItemInHand().getItemMeta().getDisplayName().equals(plugin.gravityGun.getItemMeta().getDisplayName()))) {
-
-                                    destroyFallingBlock(fallingBlock, player);
-                                    player.sendMessage(Messages.getGun_released());
-                                    cancel();
-                                }
-                            }
-                        }.runTaskTimer(plugin, 0, 1);
-
-                        lastPlayer = player;
-                        lastBlock = block;
+                    if (fallingBlockHashMap.containsKey(player)) {
+                        player.sendMessage(Messages.getAlready_using_gun());
+                        return;
                     }
+                    // Spawn le falling block
+                    FallingBlock fallingBlock = block.getWorld().spawn(block.getLocation().add(0, 0.01, 0), FallingBlock.class);
+                    fallingBlock.setGravity(false);
+                    fallingBlock.setDropItem(true);
+                    fallingBlock.setInvulnerable(true);
+                    fallingBlock.setPortalCooldown(99999999);
+                    fallingBlock.setCustomNameVisible(true);
+                    fallingBlock.setFreezeTicks(999999999);
+                    player.sendMessage(Messages.getBlock_catch());
+                    fallingBlockHashMap.put(player, fallingBlock);
+                    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_YES, 1, 1);
+                    block.setType(Material.AIR);
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            if (player.isSneaking() && player.getGameMode() != GameMode.CREATIVE) {
+                                destroyFallingBlock(fallingBlock, player);
+                                player.sendMessage(Messages.getBlock_placed_sneak());
+                                cancel();
+                                return;
+                            } else {
+                                // Spawn le laser ou non
+                                if (plugin.lazerEnabled()) {
+                                    lazer(fallingBlock, player);
+                                }
+                                // Déplace le bloc
+                                org.bukkit.Location targetLocation = player.getLocation().add(player.getLocation().getDirection().multiply(3)).add(0, 1.2, 0);
+                                org.bukkit.util.Vector velocity = targetLocation.subtract(fallingBlock.getLocation()).toVector();
+                                fallingBlock.setVelocity(velocity);
+                                // Display name du falling block
+                                Double x = fallingBlock.getLocation().getX();
+                                Double y = fallingBlock.getLocation().getY();
+                                Double z = fallingBlock.getLocation().getZ();
+                                DecimalFormat f = new DecimalFormat("##.00");
+                                fallingBlock.setCustomName("Gravity Gun - " + f.format(x) + "/" + f.format(y) + "/" + f.format(z));
+                            }
+                            if (!(player.getItemInHand() != null && player.getItemInHand().getType() == plugin.gravityGun.getType() && player.getItemInHand().hasItemMeta() && player.getItemInHand().getItemMeta().hasDisplayName() && player.getInventory().getItemInHand().getItemMeta().getDisplayName().equals(plugin.gravityGun.getItemMeta().getDisplayName()))) {
+                                destroyFallingBlock(fallingBlock, player);
+                                player.sendMessage(Messages.getGun_released());
+                                cancel();
+                            }
+                            if (fallingBlock.isOnGround()) {
+                                destroyFallingBlock(fallingBlock, player);
+                                player.sendMessage(Messages.getBlock_placed());
+                                cancel();
+                            }
+                        }
+                    }.runTaskTimer(plugin, 0, 1);
                 }
             }
         }
     }
-
     public void destroyFallingBlock(FallingBlock fallingBlock, Player player) {
-        fallingBlock.setGravity(true);
-        fallingBlock.setDropItem(true);
-        fallingBlock.setHurtEntities(true);
-        fallingBlock.setInvulnerable(false);
-        fallingBlock.setCustomNameVisible(false);
+        if (!plugin.place) {
+            fallingBlock.setGravity(true);
+            fallingBlock.setDropItem(true);
+            fallingBlock.setHurtEntities(true);
+            fallingBlock.setInvulnerable(false);
+            fallingBlock.setCustomNameVisible(false);
+        } else {
+            fallingBlock.getLocation().getBlock().setType(fallingBlock.getBlockData().getMaterial());
+            fallingBlock.remove();
+        }
         fallingBlockHashMap.remove(player);
     }
-
+    public void lazer(FallingBlock fallingBlock, Player player) {
+        if (!fallingBlockHashMap.containsKey(player)) return;
+        Location loc1 = fallingBlock.getLocation();
+        Location loc2 = player.getLocation().add(0, 0.9, 0);
+        Vector vector = getDirectionBetweenLocations(loc1, loc2).normalize(); //make sure it has length one at start
+        for (double i = 0; i <= loc1.distance(loc2); i += 0.1) {
+            Vector addition = new Vector().copy(vector).multiply(i);
+            Location newLoc = loc1.clone().add(addition).add(0, 0.5, 0);
+            loc1.getWorld().spawnParticle(Particle.REDSTONE, newLoc, 0, 0, 2, 0, 1, new Particle.DustOptions(plugin.getRGBFromConfig(), 0.5F));
+        }
+    }
 }
-
